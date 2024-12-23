7f08bcb110aec396f16f8b15c346179e6722dd08
Cestercian <yashafaid@gmail.com>
2024-12-24 00:28:39 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+
+
+
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.full})`
+    })
