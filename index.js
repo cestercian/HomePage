8f40e475e4965014b1a789cddd0b5402415388c3
Cestercian <yashafaid@gmail.com>
2024-12-24 18:31:20 +0530

    Version 1.1

--- index.js
+++ index.js
@@ -4,15 +4,54 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         document.body.style.backgroundImage = `url(${data.urls.regular})`
         document.getElementById("author").textContent = `By: ${data.user.name}`
     })
-    
-    .catch( () => {
+    .catch(err => {
         // Use a default background image/author
-        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080)`
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`
         document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
-
-fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
-    .then(res=>res.json())
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
     .then(data => {
-        document.getElementById("top1").innerText = data.market_data.current_price.inr})
+        document.getElementById("crypto-top").innerHTML = `
+            <img src=${data.image.small} />
+            <span>${data.name}</span>
+        `
+        document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `
+    })
+    .catch(err => console.error(err))
+
+// function getCurrentTime() {
+//     const date = new Date()
+//     document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+// }
+
+// setInterval(getCurrentTime, 1000)
+
+
+/**
+ * Challenge: Get the user's current weather for their area and
+ * log it to the console
+ *
+ * BaseURL: https://apis.scrimba.com/openweathermap/data/2.5/weather
+ * Queries to include:
+ *     - lat (latitude)
+ *     - lon (longitude)
+ *     - units (imperial or metric)
+ */
+
+navigator.geolocation.getCurrentPosition(position => {
+    console.log(position)
+
+});
+
